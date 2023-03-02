5b8ef80d71ee7d2cb0ef36b51a2b45e103f341ce
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-02 21:16:43 +0200

    Create B 219158- Basic Data Types.cpp

---
+++ B 219158- Basic Data Types.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,n,m) for(int i=n;i<m;i++)
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define nl "\n"
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+const int mod=998244353;
+const double pi = 3.141592653589793;
+int main()
+{
+    int n;
+    cin>>n;
+    long long m;
+    cin>>m;
+    char ch;
+    cin>>ch;
+    float f;
+    cin>>f;
+    double d;
+    cin>>d;
+    cout<<n<<endl;
+    cout<<m<<endl;
+    cout<<ch<<endl;
+    cout<<f<<endl;
+    cout<<d<<endl;
+        return 0;
+}
+
