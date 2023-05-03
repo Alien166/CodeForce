59261e72a42162bc674db061e43b1a46b4cdf7c3
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-05-03 16:31:56 +0300

    Create A - Children and Candies (ABC Edit).cpp

---
+++ A - Children and Candies (ABC Edit).cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n,sum=0;
+    cin>>n;
+    for(int i=1;i<=n;++i)
+    {
+        sum=sum+i;
+    }
+    cout<<sum;
+}
