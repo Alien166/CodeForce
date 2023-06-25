d3b7232f17afa3735e5e83083d6d801ce7dca770
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-06-25 22:29:04 +0300

    Create 688B - Lovely Palindromes.cpp

---
+++ 688B - Lovely Palindromes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+  string str;
+  cin>>str;
+  cout<<str;
+  reverse(str.begin(),str.end());
+  cout<<str;
+
+
+    return 0;
+}
