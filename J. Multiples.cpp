1894cfe458652fe4ff9f7ddb2be1aa4ae778bb6b
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-07 22:50:00 +0300

    Create J. Multiples.cpp

---
+++ J. Multiples.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+  int a,b;
+  cin>>a>>b;
+  if(a%b==0 || b%a==0) {
+      cout<<"Multiples\n";
+  } else {
+      cout<<"No Multiples\n";
+  }
+
+
+
+}
+ 
