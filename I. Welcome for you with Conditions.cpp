0cbac51ced3fb017d6b740257c93a2530449ac43
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-07 22:48:23 +0300

    Create I. Welcome for you with Conditions.cpp

---
+++ I. Welcome for you with Conditions.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+  int a,b;
+  cin>>a>>b;
+  if(a>=b) {
+      cout<<"Yes\n";
+  } else {
+      cout<<"No\n";
+  }
+
+
+
+}
+ 
