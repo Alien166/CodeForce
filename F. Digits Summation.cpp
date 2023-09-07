48b518264606d03dc5a90cc63db410cf9a8d4718
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-07 22:45:05 +0300

    Create F. Digits Summation.cpp

---
+++ F. Digits Summation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+   ll n,m;
+   cin>>n>>m;
+   int last1=n%10;
+   int last2=m%10;
+   int z=last1+last2;
+   cout<<z;
+
+
+}
