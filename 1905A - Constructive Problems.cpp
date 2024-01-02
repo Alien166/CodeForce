cf092f39cd3bbe8fe13676a7928db49c2da1099e
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-01-02 22:41:22 +0200

    Create 1905A - Constructive Problems.cpp

---
+++ 1905A - Constructive Problems.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define long long ll
+using namespace std;
+
+int main() {
+    int tc;  cin>>tc;
+    while(tc--) {
+        int n,m;
+        cin>>n>>m;
+        cout<<max(m,n)<<endl;
+    }
+
+        return 0;
+}
+
